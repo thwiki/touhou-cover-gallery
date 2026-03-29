758b61506ce2f1d0d4c953309d5cfa00088560ba
whuihuan <git@ayaka.ink>
2026-03-29 22:21:19 +0800

    新增键盘切换功能

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -1,84 +1,161 @@
-window.galleryImages = [
-    // 旧作
-    [
-        { src: 'images/game/1.jpg', alt: '东方灵异传' },
-        { src: 'images/game/2.jpg', alt: '东方封魔录' },
-        { src: 'images/game/3.jpg', alt: '东方梦时空' },
-        { src: 'images/game/4.jpg', alt: '东方幻想乡' },
-        { src: 'images/game/5.jpg', alt: '东方怪绮谈' },
-    ],
-    // 整数作（旧十年 2002~2012）
-    [
-        { src: 'images/game/6.jpg', alt: '东方红魔乡' },
-        { src: 'images/game/7.jpg', alt: '东方妖妖梦' },
-        { src: 'images/game/8.jpg', alt: '东方永夜抄' },
-        { src: 'images/game/9.jpg', alt: '东方花映塚' },
-        { src: 'images/game/10.jpg', alt: '东方风神录' },
-        { src: 'images/game/11.jpg', alt: '东方地灵殿' },
-        { src: 'images/game/12.jpg', alt: '东方星莲船' },
-        { src: 'images/game/13.jpg', alt: '东方神灵庙' },
-    ],
-    // 整数作（12年以后 2013~）
-    [
-        { src: 'images/game/14.jpg', alt: '东方辉针城' },
-        { src: 'images/game/15.jpg', alt: '东方绀珠传' },
-        { src: 'images/game/16.jpg', alt: '东方天空璋' },
-        { src: 'images/game/17.jpg', alt: '东方鬼形兽' },
-        { src: 'images/game/18.jpg', alt: '东方虹龙洞' },
-        { src: 'images/game/19.jpg', alt: '东方兽王园' },
-        { src: 'images/game/20.jpg', alt: '东方锦上京' },
-    ],
-    // 小数作
-    [
-        { src: 'images/dgame/95.jpg', alt: '东方文花帖' },
-        { src: 'images/dgame/125.jpg', alt: '东方文花帖DS' },
-        { src: 'images/dgame/128.jpg', alt: '妖精大战争' },
-        { src: 'images/dgame/143.jpg', alt: '弹幕天邪鬼' },
-        { src: 'images/dgame/165.jpg', alt: '秘封噩梦日记' },
-        { src: 'images/dgame/185.jpg', alt: '弹幕狂们的黑市' },
-    ],
-    // 黄昏作
-    [
-        { src: 'images/ogame/75.jpg', alt: '东方萃梦想' },
-        { src: 'images/ogame/105.jpg', alt: '东方绯想天' },
-        { src: 'images/ogame/123.jpg', alt: '东方非想天则' },
-        { src: 'images/ogame/135.jpg', alt: '东方心绮楼' },
-        { src: 'images/ogame/145.jpg', alt: '东方深秘录' },
-        { src: 'images/ogame/155.jpg', alt: '东方凭依华' },
-        { src: 'images/ogame/175.jpg', alt: '东方刚欲异闻' },
-    ],
-    // CD
-    [
-        { src: 'images/cd/c1.jpg', alt: '蓬莱人形' },
-        { src: 'images/cd/c2.jpg', alt: '莲台野夜行' },
-        { src: 'images/cd/c3.jpg', alt: '梦违科学世纪' },
-        { src: 'images/cd/c4.jpg', alt: '卯酉东海道' },
-        { src: 'images/cd/c5.jpg', alt: '大空魔术' },
-        { src: 'images/cd/c55.jpg', alt: '未知之花 魅知之旅' },
-        { src: 'images/cd/c6.jpg', alt: '鸟船遗迹' },
-        { src: 'images/cd/c7.jpg', alt: '伊奘诺物质' },
-        { src: 'images/cd/c8.jpg', alt: '燕石博物志' },
-        { src: 'images/cd/c9.jpg', alt: '旧约酒馆' },
-        { src: 'images/cd/c95.jpg', alt: '虹色的北斗七星' },
-        { src: 'images/cd/c10.jpg', alt: '七夕坂梦幻能' },
-    ],
-    // 旧作OST
-    [
-        { src: 'images/cd/a1.jpg', alt: '幺乐团的历史1' },
-        { src: 'images/cd/a2.jpg', alt: '幺乐团的历史2' },
-        { src: 'images/cd/a3.jpg', alt: '幺乐团的历史3' },
-        { src: 'images/cd/a4.jpg', alt: '幺乐团的历史4' },
-        { src: 'images/cd/a5.jpg', alt: '幺乐团的历史5' },
-    ],
-    // 黄昏作OST
-    [
-        { src: 'images/cd/o1.jpg', alt: '幻想曲拔萃' },
-        { src: 'images/cd/o2.jpg', alt: '全人类的天乐录' },
-        { src: 'images/cd/o3.jpg', alt: '核热造神非想天则' },
-        { src: 'images/cd/o4.jpg', alt: '暗黑能乐集心绮楼' },
-        { src: 'images/cd/o5.jpg', alt: '深秘乐曲集' },
-        { src: 'images/cd/o55.jpg', alt: '深秘乐曲集·补' },
-        { src: 'images/cd/o6.jpg', alt: '完全凭依唱片名录' },
-        { src: 'images/cd/o7.jpg', alt: '贪欲之兽的音乐' },
-    ],
-];
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            // 当前指引器横向索引
+            currentXIndex: 0,
+            // 当前指引器纵向索引
+            currentYIndex: 0,
+
+            // 是否为点击路径指示器触发的索引变化
+            isGo: false,
+
+            // 图片和路径指示器的尺寸和间距配置
+            imageOp: {
+                width: 600,
+                height: 400,
+                margin: 20
+            },
+            pathOp: {
+                width: 10,
+                height: 10,
+                marginTop: 5,
+                marginLeft: 5
+            },
+        }
+    },
+    computed: {
+        images() {
+            // 直接返回全局的galleryData
+            return window.galleryImages;
+        },
+        currentMaxXIndex() {
+            // 横向索引的最大值取决于图片数组的长度
+            return this.images.length - 1;
+        },
+        currentMaxYIndex() {
+            if (this.images.length === 0) {
+                return 0;
+            }
+            // 纵向索引的最大值取决于当前横向索引对应的图片组的长度
+            return this.images[this.currentXIndex].length - 1;
+        },
+        imageLeft() {
+            // 计算图片的left值
+            return this.imageOp.width + this.imageOp.margin;
+        },
+        imageTop() {
+            // 计算图片的top值
+            return this.imageOp.height + this.imageOp.margin;
+        },
+        pathLeft() {
+            // 计算路径指示器的left值
+            return this.pathOp.width + this.pathOp.marginLeft;
+        },
+        pathTop() {
+            // 计算路径指示器的top值
+            return this.pathOp.height + this.pathOp.marginTop;
+        }
+    },
+    watch: {
+        currentXIndex(newX) {
+            // 当横向索引变化时，重置纵向索引为当前横向索引对应的图片组的最大值
+            if (!this.isGo) {
+                this.currentYIndex = Math.min(this.currentYIndex, this.currentMaxYIndex);
+            }
+
+            // 计算新的left和top值，并更新inside的样式
+            this.onChangePos(newX, this.currentYIndex);
+        },
+        currentYIndex(newY) {
+            // 计算新的left和top值，并更新inside的样式
+            this.onChangePos(this.currentXIndex, newY);
+        }
+    },
+    methods: {
+        initGallery() {
+            // 重置横纵向索引
+            this.currentXIndex = 0;
+            this.currentYIndex = 0;
+
+            // 计算图片数组有几组
+            var xlen = this.images.length;
+
+            // 计算每组图片的位置
+            for (var i = 0; i < xlen; i++) {
+                var refItem = this.$refs['item' + i];
+                if (!refItem) {
+                    continue;
+                }
+                var t = refItem[0];
+                t.style.left = i * this.imageLeft + 'px';
+                var ylen = this.images[i].length;
+                for (var j = 0; j < ylen; j++) {
+                    var img = this.$refs['img' + i + '_' + j][0];
+                    img.style.top = j * this.imageTop + 'px';
+                }
+            }
+
+            // 监听键盘事件
+            window.addEventListener('keydown', (event) => {
+                switch (event.key) {
+                    case 'ArrowUp':
+                        this.handleUp();
+                        break;
+                    case 'ArrowDown':
+                        this.handleDown();
+                        break;
+                    case 'ArrowLeft':
+                        this.handleLeft();
+                        break;
+                    case 'ArrowRight':
+                        this.handleRight();
+                        break;
+                }
+            });
+        },
+        handleUp() {
+            // 当纵向索引大于0时，纵向索引减一
+            if (this.currentYIndex > 0) {
+                this.currentYIndex--;
+            }
+        },
+        handleDown() {
+            // 当纵向索引小于最大值时，纵向索引加一
+            if (this.currentYIndex < this.currentMaxYIndex) {
+                this.currentYIndex++;
+            }
+        },
+        handleLeft() {
+            // 当横向索引大于0时，横向索引减一
+            if (this.currentXIndex > 0) {
+                this.currentXIndex--;
+            }
+        },
+        handleRight() {
+            // 当横向索引小于最大值时，横向索引加一
+            if (this.currentXIndex < this.currentMaxXIndex) {
+                this.currentXIndex++;
+            }
+        },
+        handleGo(x, y) {
+            // 横向索引和纵向索引都设置为x和y
+            this.isGo = true;
+            this.currentXIndex = x;
+            this.currentYIndex = y;
+        },
+        onChangePos(x, y) {
+            // 计算新的left和top值，并更新inside的样式
+            var left = x * this.imageLeft;
+            var top = y * this.imageTop;
+            var inside = this.$refs.inside;
+            inside.style.left = -left + 'px';
+            inside.style.top = -top + 'px';
+            this.isGo = false;
+        }
+    },
+    mounted() {
+        // 初始化画廊
+        this.initGallery();
+    }
+});
